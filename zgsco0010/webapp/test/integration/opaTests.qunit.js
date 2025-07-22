fdee6e38a7d39d5e95b86e436983ac48699cec05
dbimgsitm <165974193+dbimgsitm@users.noreply.github.com>
2025-07-22 17:16:41 +0900

    2025.07.22 : 제조원가명세서 upload

---
+++ zgsco0010/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/gsitm/cm/zgsitmco0010test/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ zgsco0010/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comgsitmcm/zgsitmco0010_test/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
