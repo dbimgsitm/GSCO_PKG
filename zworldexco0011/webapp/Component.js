6dea000e7fbdded000300fabd12ee522abd5b74f
dbimgsitm <165974193+dbimgsitm@users.noreply.github.com>
2025-07-24 17:36:19 +0900

    2025.07.24 수불연동 평가클래스 기준정보 upload

---
+++ zworldexco0011/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.worldex.co.zworldexco0011.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
